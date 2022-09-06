164315e69fdf5f8252536cc03a6a65a433bd0206
y-satou <y-satou@shibuya.co.jp>
2022-09-06 13:35:20 +0900

    Friendly参照追加

--- FriendlyTest/MainWindow.xaml.cs
+++ FriendlyTest/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FriendlyTest
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("シブヤ魂");
+        }
     }
 }
