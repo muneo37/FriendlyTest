4ee53f856466bf0dd187366a101c1fa0428fc075
muneo37 <y.wai.ysuke0822@gmail.com>
2022-09-08 17:47:21 +0900

    タイトルの変更ができた。。

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -4,32 +4,72 @@ using System.Collections.Generic;
 using WpfApplication;
 using System.Linq;
 using UnitTestProject.Driver;
+using System.IO;
+using System.Diagnostics;
+using Codeer.Friendly;
+using RM.Friendly.WPFStandardControls;
+using System.Windows;
+using Codeer.Friendly.Dynamic;
+using System.Windows.Controls;
+using Codeer.Friendly.Windows;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class UnitTest1
     {
-        AppDriver _app;
+        WindowsAppFriend _app;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            _app = new AppDriver();
+            var dir = Path.GetFullPath("../../../../FriendlyTest/bin/x64/Debug/net6.0-windows");
+            var pathExe = dir + "\\FriendlyTest.exe";
+            //var dir = Path.GetFullPath("D:/source/repos/VisualStudio2022/WPFFriendlySampleDotNetConf2016-master/WPFFriendlySampleDotNetConf2016-master/Project/WpfApplication/bin/Release");
+            //var pathExe = dir + "/WpfApplication.exe";
+            var info = new ProcessStartInfo(pathExe) { WorkingDirectory = dir };
+            var process = Process.Start(pathExe);
+            //var targetApp = Process.GetProcessesByName("FriendlyTest")[0];
+
+            var dllPath = "C:\\Program Files\\dotnet\\shared\\Microsoft.NETCore.App\\6.0.8\\coreclr.dll";
+            _app = new WindowsAppFriend(process, dllPath);
+            //_app = new WindowsAppFriend(process, "4.0");
+            //WindowsAppExpander.LoadAssembly(_app, GetType().Assembly);
+
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-            _app.Dispose();
+            Process.GetProcessById(_app.ProcessId).Kill();
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            var entry = _app.Main;
+            // .netのタイプからウィンドウを取得
 
-            entry.button_click();
+            var window = _app.Type<Application>().Current.MainWindow;
+
+            window.Title = "タイトル変更";
+
+            var btn = window.LogicalTree().ByType<Button>().ByBinding("button1").Single();
+
+            WPFButtonBase wpfButtonBase = new WPFButtonBase(btn);
+            wpfButtonBase.EmulateClick();
+
+            // フィールドからユーザーコントロールを取得(Friendlyの基本機能)
+            //AppVar userControl = window.Dynamic()._userControl;
+
+            //// バインディングからDataGridを取得
+            //WPFDataGrid dataGrid = userControl.LogicalTree().ByBinding("SelectedItem.Value").Single().Dynamic();
+
+            //// edit.
+            //dataGrid.EmulateChangeCellText(1, 2, "abc");
+
+            //var entry = _app.Main;
+
+            //entry.button_click();
         }
     }
 }
